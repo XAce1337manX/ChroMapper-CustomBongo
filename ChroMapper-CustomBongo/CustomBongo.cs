eb9135e4f37b01755ce1a425d2c96e4bdc979912
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2024-01-20 17:16:00 +1000

    Initial commit

---
+++ ChroMapper-CustomBongo/CustomBongo.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+using System.Text.Json;
+
+public class CustomBongo : MonoBehaviour
+{
+    private const string BothFile = "both.png";
+    private const string LeftFile = "left.png";
+    private const string RightFile = "right.png";
+    private const string NoneFile = "none.png";
+    private const string ConfigFile = "config.json";
+
+    public void Start()
+    {
+        var assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        if (assemblyFolder == null)
+        {
+            Debug.LogWarning("Could not determine plugin location");
+            return;
+        }
+
+        if (!BongoImagesExist(assemblyFolder))
+        {
+            Debug.LogWarning("Not all bongo images are present");
+            return;
+        }
+        
+        var config = GetConfig(assemblyFolder);
+
+        SetCustomBongo(assemblyFolder, config);
+    }
+
+    private static Config GetConfig(string assemblyFolder)
+    {
+        var configPath = Path.Combine(assemblyFolder, ConfigFile);
+        if (!File.Exists(configPath))
+        {
+            var stream = File.Create(configPath);
+            JsonSerializer.Serialize(stream, new Config());
+            stream.Dispose();
+        }
+        
+        var configText = File.ReadAllText(configPath);
+        var config = JsonSerializer.Deserialize<Config>(configText)!;
+
+        return config;
+    }
+
+    private bool BongoImagesExist(string assemblyFolder)
+    {
+        var bothExists = File.Exists(Path.Combine(assemblyFolder, BothFile));
+        var leftExists = File.Exists(Path.Combine(assemblyFolder, LeftFile));
+        var rightExists = File.Exists(Path.Combine(assemblyFolder, RightFile));
+        var noneExists = File.Exists(Path.Combine(assemblyFolder, NoneFile));
+
+        var allExists  = bothExists && leftExists && rightExists && noneExists;
+
+        if (!allExists)
+        {
+            var message = new StringBuilder("Missing custom bongo cat images:");
+            if (!bothExists) message.Append($"\n  {BothFile}");
+            if (!leftExists) message.Append($"\n  {LeftFile}");
+            if (!rightExists) message.Append($"\n  {RightFile}");
+            if (!noneExists) message.Append($"\n  {NoneFile}");
+            PersistentUI.Instance.ShowDialogBox(message.ToString(), null, PersistentUI.DialogBoxPresetType.Ok);
+        }
+
+        return allExists;
+    }
+
+    private void SetCustomBongo(string assemblyFolder, Config config)
+    {
+        var bongoCat = FindObjectOfType<BongoCat>();
+
+        var customBongo = ScriptableObject.CreateInstance<BongoCatPreset>();
+        customBongo.LeftDownRightDown = CreateSpriteFromPath(Path.Combine(assemblyFolder, BothFile));
+        customBongo.LeftDownRightUp = CreateSpriteFromPath(Path.Combine(assemblyFolder, LeftFile));
+        customBongo.LeftUpRightDown = CreateSpriteFromPath(Path.Combine(assemblyFolder, RightFile));
+        customBongo.LeftUpRightUp = CreateSpriteFromPath(Path.Combine(assemblyFolder, NoneFile));
+        customBongo.Scale = new Vector2(config.ScaleX, config.ScaleY);
+        customBongo.YOffset = config.OffsetY;
+
+        var bongoCats = bongoCat.GetType().GetField("bongoCats", BindingFlags.NonPublic | BindingFlags.Instance);
+        var edit = (BongoCatPreset[])bongoCats.GetValue(bongoCat);
+        edit[0] = customBongo;
+        bongoCats.SetValue(bongoCat, edit);
+
+        var updateBongoCat = bongoCat.GetType()
+            .GetMethod("UpdateBongoCatState", BindingFlags.NonPublic | BindingFlags.Instance);
+        updateBongoCat.Invoke(bongoCat, new object[] { true });
+    }
+
+    private Sprite CreateSpriteFromPath(string path)
+    {
+        var bytes = File.ReadAllBytes(path);
+        var texture = new Texture2D(128, 128);
+        texture.LoadImage(bytes);
+
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), 100f);
+        return sprite;
+    }
+
+    private class Config
+    {
+        public float ScaleX { get; set; } = 1;
+        public float ScaleY { get; set; } = 1;
+        public float OffsetY { get; set; }
+    }
+}
---
+++ ChroMapper-CustomBongo/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[Plugin("ChroMapper CustomBongo")]
+public class Plugin
+{
+    private GameObject _customBongo;
+    
+    [Init]
+    private void Init()
+    {
+        Debug.Log("Init");
+        SceneManager.sceneLoaded += SceneLoaded;
+        
+        var button = new ExtensionButton
+        {
+            Tooltip = "Reload Bongo Cat",
+            Click = ReloadBongo,
+            Icon = CreateIcon()
+        };
+        ExtensionButtons.AddButton(button);
+    }
+
+    [Exit]
+    private void Exit()
+    {
+
+    }
+
+    private void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex == 3)
+        {
+            _customBongo = new GameObject("CustomBongo", typeof(CustomBongo));
+        }
+    }
+
+    private void ReloadBongo()
+    {
+        if (_customBongo != null)
+        {
+            _customBongo.GetComponent<CustomBongo>().Start();
+        }
+    }
+
+    private static Sprite CreateIcon()
+    {
+        var iconStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChroMapper-CustomBongo.refresh-icon.png")!;
+        var iconData = new byte[iconStream.Length];
+        iconStream.Read(iconData, 0, (int)iconStream.Length);
+        
+        var texture = new Texture2D(16, 16);
+        texture.LoadImage(iconData);
+
+        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2());
+    }
+}
