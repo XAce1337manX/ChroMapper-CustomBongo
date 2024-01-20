4cc0b382331d71ae8aa92c6dbc9eab270daebfdb
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2024-01-20 22:59:44 +1000

    Correct pivot

--- ChroMapper-CustomBongo/CustomBongo.cs
+++ ChroMapper-CustomBongo/CustomBongo.cs
@@ -108,7 +108,7 @@ public class CustomBongo : MonoBehaviour
         var texture = new Texture2D(128, 128);
         texture.LoadImage(bytes);
 
-        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), 100f);
+        var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100f);
         return sprite;
     }
 
